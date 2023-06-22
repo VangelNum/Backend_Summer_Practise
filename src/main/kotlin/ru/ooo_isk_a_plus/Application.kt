cce865a02fa0e2548b8bf1b379161db20b3bd641
VangelNum <vangelnum@gmail.com>
2023-06-22 18:25:12 +0400

    trying to work with railway

--- src/main/kotlin/ru/ooo_isk_a_plus/Application.kt
+++ src/main/kotlin/ru/ooo_isk_a_plus/Application.kt
@@ -4,11 +4,11 @@ import io.ktor.server.application.Application
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 import org.jetbrains.exposed.sql.Database
+import ru.ooo_isk_a_plus.plugins.configureSerialization
 import ru.ooo_isk_a_plus.routing.configureListOfUsers
 import ru.ooo_isk_a_plus.routing.configureNewUserRouting
-import ru.ooo_isk_a_plus.plugins.configureSerialization
-import java.io.FileInputStream
-import java.util.Properties
+import ru.ooo_isk_a_plus.routing.configureRouting
+
 
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -16,25 +16,16 @@ fun main() {
 }
 
 fun Application.module() {
-    val config = Properties()
-    val configFile = FileInputStream("config.properties")
-    config.load(configFile)
-    configFile.close()
-
-    val dbUrl = config.getProperty("db.url")
-    val dbDriver = config.getProperty("db.driver")
-    val dbUser = config.getProperty("db.user")
-    val dbPassword = config.getProperty("db.password")
-
     Database.connect(
-        url = dbUrl,
-        driver = dbDriver,
-        user = dbUser,
-        password = dbPassword
+        url = "jdbc:postgresql://localhost:5432/iskadatabase",
+        driver = "org.postgresql.Driver",
+        user = "postgres",
+        password = "Kefir13377"
     )
-
+    configureRouting()
     configureListOfUsers()
     configureNewUserRouting()
     configureSerialization()
 }
+
 
---
+++ src/main/kotlin/ru/ooo_isk_a_plus/routing/HelloWorldRouting.kt
@@ -0,0 +1,16 @@
+package ru.ooo_isk_a_plus.routing
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
