aba556617a9183253ab9f2df55def42bf86aebb7
VangelNum <vangelnum@gmail.com>
2023-06-22 18:52:22 +0400

    trying to work with railway 2

--- src/main/kotlin/ru/ooo_isk_a_plus/Application.kt
+++ src/main/kotlin/ru/ooo_isk_a_plus/Application.kt
@@ -1,8 +1,8 @@
 package ru.ooo_isk_a_plus
 
 import io.ktor.server.application.Application
-import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import org.jetbrains.exposed.sql.Database
 import ru.ooo_isk_a_plus.plugins.configureSerialization
 import ru.ooo_isk_a_plus.routing.configureListOfUsers
@@ -11,16 +11,21 @@ import ru.ooo_isk_a_plus.routing.configureRouting
 
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(
+        Netty,
+        port = 6654,
+        host = "containers-us-west-188.railway.app",
+        module = Application::module
+    )
         .start(wait = true)
 }
 
 fun Application.module() {
     Database.connect(
-        url = "jdbc:postgresql://localhost:5432/iskadatabase",
+        url = "jdbc:postgresql://postgres:VbBpucXWGDV201X7odWY@containers-us-west-188.railway.app:6654/railway",
         driver = "org.postgresql.Driver",
         user = "postgres",
-        password = "Kefir13377"
+        password = "VbBpucXWGDV201X7odWY"
     )
     configureRouting()
     configureListOfUsers()
